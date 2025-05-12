6a37294011fd040e7c02f4f2c335f71c72894579
BioMikeUkr <mykhayloshtopko@gmail.com>
2025-05-13 00:34:51 +0300

    example fixed

--- examples/cos_sim.c
+++ examples/cos_sim.c
@@ -17,20 +17,21 @@ int main() {
     get_l2_norm(v_1, v_1_l2_norm);
     get_l2_norm(v_2, v_2_l2_norm);
 
-    printf("v_1 l2 norm: ");
-    print_point(v_1_l2_norm);
-    printf("\n");
-    printf("v_2 l2 norm: ");
-    print_point(v_2_l2_norm);
-    printf("\n");
+    printf("v_1 l2 norm:\n");
+    print_obj(v_1_l2_norm);
+    printf("v_2 l2 norm:\n");
+    print_obj(v_2_l2_norm);
 
     div_vector_by_point(v_1, v_1_l2_norm, v_1);
-    print_obj(v_1);
     div_vector_by_point(v_2, v_2_l2_norm, v_2);
+
+    printf("v_1 normalized:\n");
+    print_obj(v_1);
+    printf("v_2 normalized:\n");
     print_obj(v_2);
 
     mat_mul_vector(v_1, v_2, mmul_res);
-    printf("v_1 & v_2 cosine similarity: ");
+    printf("v_1 & v_2 cosine similarity:\n");
     print_obj(mmul_res);
     printf("\n");
 
